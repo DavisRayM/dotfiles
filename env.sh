91045ed5fd49f6a1255a41be41f10456398f6ead
Davis Raymond Muro <davisraymondmuro@outlook.com>
2022-11-02 19:32:10 +0300

    Update

--- env.sh
+++ env.sh
@@ -1,6 +1,11 @@
 #!/bin/zsh
 
-export EDITOR="nvim"
+export GPG_TTY=$(tty)
+export TERM=xterm-256color
+
+# Prezto history-substring-search binding fix
+bindkey "$terminfo[kcuu1]" history-substring-search-up
+bindkey "$terminfo[kcud1]" history-substring-search-down
 
 # Add Rust binaries to path
 export PATH=$PATH:~/.cargo/bin
@@ -8,5 +13,5 @@ export PATH=$PATH:~/.cargo/bin
 # Setup zoxide for zsh
 eval "$(zoxide init zsh)"
 
-# Aliases
-alias vim="nvim"
+export EDITOR="nvim"
+export VISUAL="nvim"
