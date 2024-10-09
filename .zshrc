82cdadb683579e5c339c054c312f00c2f02893dc
Davis Raymond Muro <davis.ray.muro@gmail.com>
2024-10-09 02:20:33 -0700

    add current dotfiles

---
+++ .zshrc
@@ -0,0 +1,43 @@
+export PATH=$HOME/.cargo/env:$HOME/.config/emacs/bin:$PATH
+
+# Path to your Oh My Zsh installation.
+export ZSH="$HOME/.oh-my-zsh"
+
+# See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
+ZSH_THEME="sorin"
+
+zstyle ':omz:update' mode reminder
+zstyle ':omz:update' frequency 13
+
+# Uncomment the following line if pasting URLs and other text is messed up.
+# DISABLE_MAGIC_FUNCTIONS="true"
+
+# Uncomment the following line to disable auto-setting terminal title.
+# DISABLE_AUTO_TITLE="true"
+
+# Uncomment the following line to enable command auto-correction.
+# ENABLE_CORRECTION="true"
+
+# Uncomment the following line to display red dots whilst waiting for completion.
+# You can also set it to another string to have that shown instead of the default red dots.
+# e.g. COMPLETION_WAITING_DOTS="%F{yellow}waiting...%f"
+# Caution: this setting can cause issues with multiline prompts in zsh < 5.7.1 (see #5765)
+# COMPLETION_WAITING_DOTS="true"
+
+# Uncomment the following line if you want to disable marking untracked files
+# under VCS as dirty. This makes repository status check for large repositories
+# much, much faster.
+# DISABLE_UNTRACKED_FILES_DIRTY="true"
+
+plugins=(git history history-substring-search)
+
+source $ZSH/oh-my-zsh.sh
+
+# User configuration
+
+export EDITOR=emacs
+export GPG_TTY=$(tty)
+
+eval "$(zoxide init zsh)"
+
+source /usr/share/nvm/init-nvm.sh
---
+++ setup_wm.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+
+# Inspired by: https://github.com/SolDoesTech/hyprland/blob/main/set-hypr
+# This script attempts to setup a Hyprland environment on an Arch Linux Installation.
+# Installs:
+# - fd: Find alternative; Doomemacs requirement
+# - ripgrep: Search tool
+# - zoxide: Smarter cd
+# - ispell: Spelling checker
+# - cmake: Build tool
+# - hyprland: Tiling wayland compositor; https://github.com/hyprwm/Hyprland
+# - kitty: GPU based terminal emulator; https://github.com/kovidgoyal/kitty
+# - waybar: Wayland bar for compositors; https://github.com/Alexays/Waybar
+# - swagbg: Wallpaper tool for Wayland compositors; https://github.com/swaywm/swaybg
+# - emacs-nativecomp: EVIL Mode ❤
+# - wofi: Application launcher
+# - ttf-jetbrains-mono-nerd: Font
+# - noto-fonts-emoji: Emoji font
+# - polkit-gnome: authentication utility
+# - pamixer: Audio device settings helper
+# - bluez & bluez-utils: Bluetooth services
+# - xdg-desktop-portal-hyprland: XDG Desktop Portal backend for Hyprland; https://github.com/hyprwm/xdg-desktop-portal-hyprland
+# - zsh & Oh-My-Zsh: Unix shell; https://github.com/ohmyzsh/ohmyzsh
+# - doomemacs: Emacs framework; https://github.com/doomemacs/doomemacs
+# - google-chrome: Browser
+# - man: Manual pages
+# - tldr: Man pages are too long!!; https://github.com/tldr-pages/tldr
+# - Coding tools: clang, shellcheck, shfmt, gcc, nvm, uv, pip, python-black,
+#   python-pyflakes python-isort python-pipenv python-nose python-pytest python-setuptools
+# - [Optional] Asus ROG Utilities
+#   (asusctl, supergfxctl, rog-control-center, power-profiles-daemon); https://asus-linux.org/guides/arch-guide/
+
+YAY=/sbin/yay
+
+if [ -f "$YAY" ]; then
+    echo -e "-> Updating local pacman database...\n"
+    yay -Syu
+else
+    echo -e "yay has not been installed!\n"
+    exit
+fi
+
+echo -e "-> Installing required packages; See script for list of installed packages...\n"
+sleep 5
+yay -S --noconfirm hyprland kitty waybar swagbg \
+    emacs-nativecomp wofi ttf-jetbrains-mono-nerd \
+    noto-fonts-emoji pamixer bluez blues-utils \
+    xdg-desktop-portal-hyprland zsh google-chrome man tldr \
+    ripgrep zoxide openssh ispell cmake nvm clang shellcheck \
+    shfmt gcc fd uv python-pip python-black python-pyflakes \
+    python-isort python-pipenv python-nose python-pytest \
+    python-setuptools
+
+echo -e "-> Starting bluetooth service...\n"
+sudo systemctl enable --now bluetooth.service
+sleep 5
+
+echo -e "-> Copying configuration files for Hyprland & Waybar...\n"
+cp -R hypr ~/.config/
+cp -R waybar ~/.config/
+chmod +x ~/.config/hypr/xdg-portal-hyprland
+sleep 5
+
+echo -e "-> Setting zsh as default shell...\n"
+chsh --shell /usr/bin/zsh
+
+echo -e "-> Installing Oh-My-Zsh...\n"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sleep 5
+
+echo -e "-> Installing doomemacs...\n"
+git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs
+~/.config/emacs/bin/doom install
+sleep 5
+
+echo -e "-> Copying configuration files for zsh and emacs...\n"
+cp .zshrc ~/
+cp --update=all -R doom ~/.config/
+sleep 5
+
+echo -e "-> Installing latest stable node version...\n"
+source /usr/share/nvm/init-nvm.sh
+nvm install stable
+sleep 5
+
+read -n1 -rep 'Would you like to install Asus ROG software support? (y,n)' ROG
+if [[ $ROG == "Y" || $ROG == "y" ]]; then
+    echo -e "-> Adding g14 repo sign keys...\n"
+    sudo pacman-key --recv-keys 8F654886F17D497FEFE3DB448B15A6B0E9A3FA35
+    sudo pacman-key --finger 8F654886F17D497FEFE3DB448B15A6B0E9A3FA35
+    sudo pacman-key --lsign-key 8F654886F17D497FEFE3DB448B15A6B0E9A3FA35
+    sudo pacman-key --finger 8F654886F17D497FEFE3DB448B15A6B0E9A3FA35
+
+    LOC="/etc/pacman.conf"
+    echo -e "-> Updating $LOC with g14 repo.\n"
+    echo -e "\n[g14]\nServer = https://arch.asus-linux.org" | sudo tee -a $LOC
+    echo -e "\n"
+
+    echo -e "-> Updating local pacman database...\n"
+    sudo pacman -Suy
+
+    echo -e "-> Installing ROG pacakges...\n"
+    sudo pacman -S --noconfirm asusctl power-profiles-daemon supergfxctl rog-control-center
+    echo -e "-> Activating ROG services...\n"
+    sudo systemctl enable --now power-profiles-daemon.service
+    sleep 5
+    sudo systemctl enable --now supergfxd
+    sleep 5
+fi
+
+echo -e "\n\n-> Setup complete!! Remember to run $(doom sync)"
+sleep 10
+
+read -n1 -rep 'Would you like to start Hyprland ? (Y|N)' START
+if [[ $START == "Y" || $START == "y" ]]; then
+    exec Hyprland
+else
+    exit
+fi
