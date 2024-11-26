683a64702a54597ff05c8df8b3934d4f266ba4c1
Davis Raymond Muro <davis.ray.muro@gmail.com>
2024-11-25 18:07:15 -0800

    show notification only when discharging

--- hypr/battery_notification.sh
+++ hypr/battery_notification.sh
@@ -2,7 +2,8 @@
 
 while true; do
     bat_lvl=$(cat /sys/class/power_supply/BAT0/capacity)
-    if [ "$bat_lvl" -le 30 ]; then
+    status=$(cat /sys/class/power_supply/BAT0/status)
+    if [ "$bat_lvl" -le 30 ] && [ "$status" = "Discharging" ]; then
         notify-send --urgency=CRITICAL "Battery Low" "Level: ${bat_lvl}%"
         sleep 1200
     else
