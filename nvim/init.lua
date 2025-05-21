07cc1af77a1d15bec2c8bb269adbfc9efe0bdfac
Davis Muro <git@davisraym.com>
2025-05-21 05:00:24 -0700

    refactor: location list instead of quickfix

--- nvim/init.lua
+++ nvim/init.lua
@@ -103,7 +103,7 @@ vim.opt.confirm = true
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 -- Diagnostic keymaps
-vim.keymap.set("n", "<leader>q", vim.diagnostic.setqflist, { desc = "Open diagnostic [Q]uickfix list" })
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 vim.keymap.set("n", "<M-j>", "<cmd>cnext<CR>", { desc = "Goto next quickfix list line" })
 vim.keymap.set("n", "<M-k>", "<cmd>cprev<CR>", { desc = "Goto previous quickfix list line" })
 
