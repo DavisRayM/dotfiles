fc81801514812144629fcf7589658aa8ff69b7a8
Davis Muro <git@davisraym.com>
2025-05-21 07:45:21 -0700

    changes: blink configuration

--- nvim/init.lua
+++ nvim/init.lua
@@ -503,18 +503,16 @@ require("lazy").setup({
   { -- Autocompletion
     "saghen/blink.cmp",
     event = "VimEnter",
+    version = "1.*",
     opts = {
       keymap = {
-        preset = "enter",
+        preset = "default",
       },
-
       appearance = {
         nerd_font_variant = "mono",
       },
-
       completion = {
-        -- By default, you may press `<c-space>` to show the documentation.
-        documentation = { auto_show = true, auto_show_delay_ms = 500 },
+        documentation = { auto_show = false },
       },
 
       sources = {
@@ -523,16 +521,7 @@ require("lazy").setup({
           lazydev = { module = "lazydev.integrations.blink", score_offset = 100 },
         },
       },
-      -- Blink.cmp includes an optional, recommended rust fuzzy matcher,
-      -- which automatically downloads a prebuilt binary when enabled.
-      --
-      -- By default, we use the Lua implementation instead, but you may enable
-      -- the rust implementation via `'prefer_rust_with_warning'`
-      --
-      -- See :h blink-cmp-config-fuzzy for more information
-      fuzzy = { implementation = "lua" },
-
-      -- Shows a signature help window while you type arguments for a function
+      fuzzy = { implementation = "prefer_rust_with_warning" },
       signature = { enabled = true },
     },
   },
