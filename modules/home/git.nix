07979ec3cbdf73a7b8917b827ca6939d4f3488dd
davisraym <git@davisraym.com>
2025-08-07 08:33:59 -0700

    feat(neovim): some nifty aliases
    From: https://micahkepe.com/blog/gitconfig/

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -35,6 +35,13 @@
       interactive = {diffFilter = "delta --color-only";};
       merge = {conflictStyle = "zdiff3";};
       user = {signingkey = "${gitSigningKey}";};
+      pull = {rebase = true;};
+      push = {autoSetupRemote = true;};
+      alias = {
+        ignore = "!gi() { local IFS=','; curl -sL https://www.toptal.com/developers/gitignore/api/\"$*\"; }; gi";
+        graph = "log --all --graph --decorate --oneline";
+        l = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+      };
     };
   };
 }
