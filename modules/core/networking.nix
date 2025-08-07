34447ad19f765e0f8e2e4306999bd6f9f298c53d
davisraym <git@davisraym.com>
2025-08-07 10:59:16 -0700

    feat(networking): add networkmanagerapplet

--- modules/core/networking.nix
+++ modules/core/networking.nix
@@ -1,4 +1,10 @@
-{hostname, ...}: {
+{
+  pkgs,
+  hostname,
+  ...
+}: {
   networking.hostName = "${hostname}";
   networking.networkmanager.enable = true;
+
+  environment.systemPackages = with pkgs; [networkmanagerapplet];
 }
