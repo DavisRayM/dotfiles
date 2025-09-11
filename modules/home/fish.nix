0240debe8a1c5579badb66fcce9c2e803685379c
davisraym <git@davisraym.com>
2025-09-11 01:51:52 -0700

    feat(keymaps): enable vi-mode bindings

--- modules/home/fish.nix
+++ modules/home/fish.nix
@@ -35,11 +35,35 @@
       set __fish_git_prompt_showupstream "none"
       set __fish_git_prompt_use_informative_chars "yes"
       set -g fish_prompt_pwd_dir_length 3
+      set -g fish_key_bindings fish_vi_key_bindings
+
+      function fish_mode_prompt;end
 
       function fish_prompt
-        echo
         set_color blue
+        echo -n '['
+        switch $fish_bind_mode
+          case default
+            set_color --bold red
+            echo -n 'C'
+          case insert
+            set_color --bold green
+            echo -n 'I'
+          case replace_one
+            set_color --bold green
+            echo -n 'R'
+          case visual
+            set_color --bold brmagenta
+            echo -n 'V'
+          case '*'
+            set_color --bold red
+            echo -n '?'
+        end
+        set_color blue
+        echo -n '] '
+
         echo -n (hostnamectl hostname)
+
         if [ $PWD != $HOME ]
                 set_color brblack
                 echo -n ":"
