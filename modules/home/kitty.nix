ee951acb11a5c375563793f1ac5e36f1bfdd146b
davisraym <git@davisraym.com>
2025-09-11 12:06:11 -0700

    feat: window management keybindings

--- modules/home/kitty.nix
+++ modules/home/kitty.nix
@@ -5,6 +5,9 @@
       "ctrl+shift+6" = "send_text ctrl+6";
       "ctrl+shift+l" = "next_tab";
       "ctrl+shift+h" = "previous_tab";
+      "ctrl+shift+enter" = "launch --cwd=current";
+      "ctrl+l" = "next_window";
+      "ctrl+h" = "previous_window";
     };
   };
 }
