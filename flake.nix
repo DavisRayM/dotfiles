2fad5d17814ba88dffc75aa5ad771debc8116370
Davis Raymond Muro <git@davisraym.com>
2026-03-12 17:05:21 -0700

    nix-index-database

--- flake.nix
+++ flake.nix
@@ -3,17 +3,27 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
     hardware.url = "github:nixos/nixos-hardware/master";
+
+    index-db.url = "github:nix-community/nix-index-database";
+    index-db.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
-    { nixpkgs, hardware, ... }@inputs:
+    {
+      nixpkgs,
+      hardware,
+      index-db,
+      ...
+    }@inputs:
     {
       nixosConfigurations.blaze = nixpkgs.lib.nixosSystem {
         specialArgs = { inherit inputs; };
         modules = [
           hardware.nixosModules.asus-rog-strix-g513im
           ./configuration.nix
+          index-db.nixosModules.default
         ];
       };
     };
