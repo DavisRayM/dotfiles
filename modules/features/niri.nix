88b6d4301fbca3d253a05fc162109f3a9716d01f
Davis Raymond Muro <git@davisraym.com>
2026-04-10 22:46:57 -0700

    add overview binding

--- modules/features/niri.nix
+++ modules/features/niri.nix
@@ -120,90 +120,73 @@
           ];
 
           binds = {
-            "Mod+Return".spawn-sh = lib.getExe pkgs.kitty;
-            "Mod+S".set-dynamic-cast-window = _: { };
-            "Mod+Shift+S".set-dynamic-cast-monitor = _: { };
-            "Mod+Ctrl+Shift+S".clear-dynamic-cast-target = _: { };
+            "Alt+Print".screenshot-window = _: { };
+            "Ctrl+Print".screenshot-screen = _: { };
+
             "Mod+B".spawn-sh = "google-chrome-stable";
-            "Mod+E".spawn-sh = "${lib.getExe' pkgs.emacs "emacsclient"} -c";
-            "Mod+D".spawn-sh = "${lib.getExe self'.packages.myNoctalia} ipc call launcher toggle";
-            "XF86AudioRaiseVolume".spawn-sh =
-              "${lib.getExe' pkgs.wireplumber "wpctl"} set-volume @DEFAULT_AUDIO_SINK@ 0.1+ -l 1.0";
-
-            "XF86AudioLowerVolume".spawn-sh =
-              "${lib.getExe' pkgs.wireplumber "wpctl"} set-volume @DEFAULT_AUDIO_SINK@ 0.1-";
-
-            "XF86AudioMute".spawn-sh =
-              "${lib.getExe' pkgs.wireplumber "wpctl"} set-mute @DEFAULT_AUDIO_SINK@ toggle";
-
-            "XF86AudioMicMute".spawn-sh =
-              "${lib.getExe' pkgs.wireplumber "wpctl"} set-mute @DEFAULT_AUDIO_SOURCE@ toggle";
-
-            "XF86AudioPlay".spawn-sh = "${lib.getExe pkgs.playerctl} play-pause";
-
-            "XF86AudioPause".spawn-sh = "${lib.getExe pkgs.playerctl} pause";
-
-            "XF86AudioNext".spawn-sh = "${lib.getExe pkgs.playerctl} next";
-
-            "XF86AudioPrev".spawn-sh = "${lib.getExe pkgs.playerctl} previous";
-
-            "XF86MonBrightnessUp".spawn-sh = "${lib.getExe pkgs.brightnessctl} --class=backlight set +10%";
-            "XF86MonBrightnessDown".spawn-sh = "${lib.getExe pkgs.brightnessctl} --class=backlight set 10%-";
-
-            "XF86KbdBrightnessUp".spawn-sh = "${lib.getExe pkgs.brightnessctl} -d asus::kbd_backlight set 5%+";
-
-            "XF86KbdBrightnessDown".spawn-sh =
-              "${lib.getExe pkgs.brightnessctl} -d asus::kbd_backlight set 5%-";
-
-            "Mod+Q".close-window = _: { };
-
-            "Mod+H".focus-column-left = _: { };
-            "Mod+J".focus-window-down = _: { };
-            "Mod+K".focus-window-up = _: { };
-            "Mod+L".focus-column-right = _: { };
-
             "Mod+Ctrl+H".move-column-left = _: { };
             "Mod+Ctrl+J".move-window-down = _: { };
             "Mod+Ctrl+K".move-window-up = _: { };
             "Mod+Ctrl+L".move-column-right = _: { };
-
-            "Mod+Shift+H".focus-monitor-left = _: { };
-            "Mod+Shift+J".focus-monitor-down = _: { };
-            "Mod+Shift+K".focus-monitor-up = _: { };
-            "Mod+Shift+L".focus-monitor-right = _: { };
-
-            "Mod+Shift+Ctrl+Left".move-column-to-monitor-left = _: { };
+            "Mod+Ctrl+R".reset-window-height = _: { };
+            "Mod+Ctrl+Shift+F".toggle-windowed-fullscreen = _: { };
+            "Mod+Ctrl+Shift+S".clear-dynamic-cast-target = _: { };
+            "Mod+D".spawn-sh = "${lib.getExe self'.packages.myNoctalia} ipc call launcher toggle";
+            "Mod+E".spawn-sh = "${lib.getExe' pkgs.emacs "emacsclient"} -c";
+            "Mod+Equal".set-column-width = "+10%";
+            "Mod+F".fullscreen-window = _: { };
+            "Mod+H".focus-column-left = _: { };
+            "Mod+J".focus-window-down = _: { };
+            "Mod+K".focus-window-up = _: { };
+            "Mod+L".focus-column-right = _: { };
+            "Mod+M".maximize-window-to-edges = _: { };
+            "Mod+Minus".set-column-width = "-10%";
+            "Mod+O".open-overview = _: { };
+            "Mod+Q".close-window = _: { };
+            "Mod+R".switch-preset-column-width = _: { };
+            "Mod+Return".spawn-sh = lib.getExe pkgs.kitty;
+            "Mod+S".set-dynamic-cast-window = _: { };
             "Mod+Shift+Ctrl+Down".move-column-to-monitor-down = _: { };
-            "Mod+Shift+Ctrl+Up".move-column-to-monitor-up = _: { };
-            "Mod+Shift+Ctrl+Right".move-column-to-monitor-right = _: { };
-
             "Mod+Shift+Ctrl+H".move-column-to-monitor-left = _: { };
             "Mod+Shift+Ctrl+J".move-column-to-monitor-down = _: { };
             "Mod+Shift+Ctrl+K".move-column-to-monitor-up = _: { };
             "Mod+Shift+Ctrl+L".move-column-to-monitor-right = _: { };
-
-            "Mod+F".fullscreen-window = _: { };
+            "Mod+Shift+Ctrl+Left".move-column-to-monitor-left = _: { };
+            "Mod+Shift+Ctrl+Right".move-column-to-monitor-right = _: { };
+            "Mod+Shift+Ctrl+Up".move-column-to-monitor-up = _: { };
+            "Mod+Shift+Equal".set-window-height = "+10%";
+            "Mod+Shift+Escape".quit = _: { };
             "Mod+Shift+F".expand-column-to-available-width = _: { };
-            "Mod+Ctrl+Shift+F".toggle-windowed-fullscreen = _: { };
-            "Mod+M".maximize-window-to-edges = _: { };
-
-            "Mod+V".toggle-window-floating = _: { };
+            "Mod+Shift+H".focus-monitor-left = _: { };
+            "Mod+Shift+J".focus-monitor-down = _: { };
+            "Mod+Shift+K".focus-monitor-up = _: { };
+            "Mod+Shift+L".focus-monitor-right = _: { };
+            "Mod+Shift+Minus".set-window-height = "-10%";
+            "Mod+Shift+R".switch-preset-window-height = _: { };
+            "Mod+Shift+S".set-dynamic-cast-monitor = _: { };
+            "Mod+Shift+Slash".show-hotkey-overlay = _: { };
             "Mod+Shift+V".switch-focus-between-floating-and-tiling = _: { };
+            "Mod+V".toggle-window-floating = _: { };
 
             "Print".screenshot = _: { };
-            "Ctrl+Print".screenshot-screen = _: { };
-            "Alt+Print".screenshot-window = _: { };
 
-            "Mod+Shift+Escape".quit = _: { };
-            "Mod+Shift+Slash".show-hotkey-overlay = _: { };
-
-            "Mod+R".switch-preset-column-width = _: { };
-            "Mod+Shift+R".switch-preset-window-height = _: { };
-            "Mod+Minus".set-column-width = "-10%";
-            "Mod+Equal".set-column-width = "+10%";
-            "Mod+Shift+Minus".set-window-height = "-10%";
-            "Mod+Shift+Equal".set-window-height = "+10%";
-            "Mod+Ctrl+R".reset-window-height = _: { };
+            "XF86AudioLowerVolume".spawn-sh =
+              "${lib.getExe' pkgs.wireplumber "wpctl"} set-volume @DEFAULT_AUDIO_SINK@ 0.1-";
+            "XF86AudioMicMute".spawn-sh =
+              "${lib.getExe' pkgs.wireplumber "wpctl"} set-mute @DEFAULT_AUDIO_SOURCE@ toggle";
+            "XF86AudioMute".spawn-sh =
+              "${lib.getExe' pkgs.wireplumber "wpctl"} set-mute @DEFAULT_AUDIO_SINK@ toggle";
+            "XF86AudioNext".spawn-sh = "${lib.getExe pkgs.playerctl} next";
+            "XF86AudioPause".spawn-sh = "${lib.getExe pkgs.playerctl} pause";
+            "XF86AudioPlay".spawn-sh = "${lib.getExe pkgs.playerctl} play-pause";
+            "XF86AudioPrev".spawn-sh = "${lib.getExe pkgs.playerctl} previous";
+            "XF86AudioRaiseVolume".spawn-sh =
+              "${lib.getExe' pkgs.wireplumber "wpctl"} set-volume @DEFAULT_AUDIO_SINK@ 0.1+ -l 1.0";
+            "XF86KbdBrightnessDown".spawn-sh =
+              "${lib.getExe pkgs.brightnessctl} -d asus::kbd_backlight set 5%-";
+            "XF86KbdBrightnessUp".spawn-sh = "${lib.getExe pkgs.brightnessctl} -d asus::kbd_backlight set 5%+";
+            "XF86MonBrightnessDown".spawn-sh = "${lib.getExe pkgs.brightnessctl} --class=backlight set 10%-";
+            "XF86MonBrightnessUp".spawn-sh = "${lib.getExe pkgs.brightnessctl} --class=backlight set +10%";
           };
         };
       };
