226333524080574bd99f5978b3de641e08c4aedd
Davis Raymond Muro <git@davisraym.com>
2026-06-22 19:07:02 -0700

    flake: remove stylix

--- flake.nix
+++ flake.nix
@@ -9,9 +9,6 @@
     nix-index-database.url = "github:nix-community/nix-index-database";
     nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
 
-    stylix.url = "github:/nix-community/stylix";
-    stylix.inputs.nixpkgs.follows = "nixpkgs";
-
     flake-parts.url = "github:hercules-ci/flake-parts";
     import-tree.url = "github:vic/import-tree";
 
